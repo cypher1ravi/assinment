3ff1ac0dee0ca76e13bcb94e12f20f0376a61a9e
ravindra <123213831+cypher1ravi@users.noreply.github.com>
2023-04-27 19:05:50 +0530

    Create PersonInfo.js
    Create a console application that displays information about a person task
     Aswer

---
+++ PersonInfo.js
@@ -0,0 +1,17 @@
+Create a console application that displays information about a person.
+
+
+//get the person imformation using prompt
+
+let Name = "Ravindra Chabey"
+let age = Number(22)
+let gender = "Male"
+let Hobbies = ["playing cricket", "listing song", "watching movie"]
+let FavColor = "black"
+
+// print the person information
+console.log(`Your name is ${Name}`)
+console.log(`you age is ${age} `)
+console.log(`you are is ${gender} `)
+console.log(`you Hobbies is ${Hobbies} `)
+console.log(`your favorite color is ${FavColor} `)
